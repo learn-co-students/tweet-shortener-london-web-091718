c70d7a54619eec834fda86652e84fc7b5770e1ff
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-04 17:17:44 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,6 @@
-sub = {
+require 'pry'
+ sub = {
+
   "hello" => "hi",
   "to" => "2", 
   "two" => "2", 
@@ -26,10 +28,60 @@ def word_substituter(tweet)
   "and" => "&"
   }
 
-  tweet.each_char do |word|
-    if word.to_s.include?(sub.keys)
-     puts "jhfc"
-    
+split_tweet = []
+
+  tweet.split.each do |word|
+    if sub.has_key? word
+      split_tweet.push(sub[word])
+    else
+      split_tweet.push(word)
     end
   end
-end  
+  split_tweet.join(" ")
+end
+
+
+def bulk_tweet_shortener(array_tweets)
+  
+  sub = {
+  "hello" => "hi",
+  "to" => "2", 
+  "two" => "2", 
+  "too" => "2", 
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+  }
+  
+split_tweet = []
+
+array_tweets.each do |tweet|
+  tweet.split.each do |word|
+    if sub.has_key? (word.downcase)
+      split_tweet.push(sub[word.downcase])
+    else
+      split_tweet.push(word)
+    end
+  end
+  puts split_tweet.join(" ")
+  split_tweet = []
+end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+     word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  short_tweet = selective_tweet_shortener(tweet)
+  if short_tweet.length >= 140
+  
+  
+end
