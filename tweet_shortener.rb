26d275b980554a5997c25088a620344a613976de
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-04 10:54:12 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+sub = {
+  "hello" => "hi",
+  "to" => "2", 
+  "two" => "2", 
+  "too" => "2", 
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+
+def word_substituter(tweet)
+  
+  sub = {
+  "hello" => "hi",
+  "to" => "2", 
+  "two" => "2", 
+  "too" => "2", 
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+  }
+
+  tweet.each_char do |word|
+    if word.to_s.include?(sub.keys)
+     puts "jhfc"
+    
+    end
+  end
+end  
